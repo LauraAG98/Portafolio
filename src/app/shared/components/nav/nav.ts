e9a2968c480791f451c32ff867206cd51acfd8b3
LauraAG98 <lauraguirregarcia@outlook.com>
2025-09-15 10:10:19 -0500

    Se inicia agregando menu desplegable

--- src/app/shared/components/nav/nav.ts
+++ src/app/shared/components/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,5 +8,10 @@ import { RouterModule } from '@angular/router';
   styleUrl: './nav.css'
 })
 export class Nav {
+  protected readonly menuDesplegable = signal(false);
 
+
+  toggleElemento(){
+    this.menuDesplegable.update(valor=>!valor)
+  }
 }
