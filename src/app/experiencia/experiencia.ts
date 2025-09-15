89f04052905342297c4bf2891018e4bcba5726a9
LauraAG98 <lauraguirregarcia@outlook.com>
2025-09-15 08:35:08 -0500

    Se agrega nuevo componente (descarga)

--- src/app/experiencia/experiencia.ts
+++ src/app/experiencia/experiencia.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
 import { HeaderDos } from '../shared/components/header-dos/header-dos';
+import { Descarga } from '../shared/components/descarga/descarga';
 
 @Component({
   selector: 'app-experiencia',
-  imports: [Nav,HeaderDos,Footer],
+  imports: [Nav,HeaderDos,Descarga,Footer],
   templateUrl: './experiencia.html',
   styleUrl: './experiencia.css'
 })
---
+++ src/app/shared/components/descarga/descarga.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descarga',
+  imports: [],
+  templateUrl: './descarga.html',
+  styleUrl: './descarga.css'
+})
+export class Descarga {
+
+}
