d35fb1f7d78bb41eb6c55ff9939fc25321078bd5
LauraAG98 <lauraguirregarcia@outlook.com>
2025-09-19 14:19:10 -0500

    AGREGADA traduccion de componente blog

--- src/app/blog/blog.ts
+++ src/app/blog/blog.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
 import { HeaderPrincipal } from '../shared/components/header-principal/header-principal';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-blog',
-  imports: [Nav,HeaderPrincipal,Footer],
+  imports: [Nav,HeaderPrincipal,Footer,TranslatePipe],
   templateUrl: './blog.html',
   styleUrl: './blog.css'
 })
