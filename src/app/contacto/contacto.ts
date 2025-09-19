f1c286c9fbfd56fc4d691da0f05b1a5514e3f050
LauraAG98 <lauraguirregarcia@outlook.com>
2025-09-18 23:36:24 -0500

    AGREGADA traduccion en los header

--- src/app/contacto/contacto.ts
+++ src/app/contacto/contacto.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
 import { HeaderPrincipal } from '../shared/components/header-principal/header-principal';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contacto',
-  imports: [Nav,HeaderPrincipal,Footer],
+  imports: [Nav,HeaderPrincipal,Footer,TranslatePipe],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css'
 })
