11c73b5a6c738b8a56cdc584e4c4d74f07a45c03
LauraAG98 <lauraguirregarcia@outlook.com>
2025-09-18 23:30:23 -0500

    AGREGADA traduccion en componentes: portafolio, footer

--- src/app/experiencia/experiencia.ts
+++ src/app/experiencia/experiencia.ts
@@ -3,10 +3,11 @@ import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
 import { Descarga } from '../shared/components/descarga/descarga';
 import { HeaderPrincipal } from '../shared/components/header-principal/header-principal';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-experiencia',
-  imports: [Nav,HeaderPrincipal,Descarga,Footer],
+  imports: [Nav,HeaderPrincipal,Descarga,Footer,TranslatePipe],
   templateUrl: './experiencia.html',
   styleUrl: './experiencia.css'
 })
--- src/app/portafolio/portafolio.ts
+++ src/app/portafolio/portafolio.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
 import { HeaderPrincipal } from '../shared/components/header-principal/header-principal';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portafolio',
-  imports: [Nav,HeaderPrincipal,Footer],
+  imports: [Nav,HeaderPrincipal,Footer,TranslatePipe],
   templateUrl: './portafolio.html',
   styleUrl: './portafolio.css'
 })
